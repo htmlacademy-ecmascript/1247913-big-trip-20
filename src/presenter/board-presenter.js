e4b31dd237b0d84899a7029008484b3103c12723
Yan Shatskiy <60981097+jan-web@users.noreply.github.com>
2023-05-05 13:09:59 +0200

    feat: The module 1 fineshed

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,9 +1,9 @@
 
-import SortView from '../view/sort-view';
-import {render} from '../framework/render.js';
-import TripEventsListView from '../view/trip-events-list-view';
-import EditPointView from '../view/edit-event-view';
-import PointView from '../view/point-view';
+import { render } from '../framework/render.js';
+import EditPointView from '../views/edit-event-view';
+import PointView from '../views/point-view';
+import SortView from '../views/sort-view';
+import TripEventsListView from '../views/trip-events-list-view';
 
 export default class BoardPresenter {
   sortComponent = new SortView();
@@ -11,11 +11,15 @@ export default class BoardPresenter {
 
   editPointForm = new EditPointView();
 
-  constructor({boardContainer}) {
+  constructor({boardContainer, pointsModel}) {
     this.boardContainer = boardContainer;
+    this.pointsModel = pointsModel;
   }
 
   init() {
+    this.boardPoints = [...this.pointsModel.getPoints()];
+    console.log('this.boardPoints: ', this.boardPoints);
+
     render(this.sortComponent, this.boardContainer);
     render(this.tripEventsListComponent, this.boardContainer);
     render(new EditPointView(), this.tripEventsListComponent.getElement());
