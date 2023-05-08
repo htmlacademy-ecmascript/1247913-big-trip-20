067dd1b732a5831b336c5266426795f05c980cf0
Yan Shatskiy <60981097+jan-web@users.noreply.github.com>
2023-05-08 07:37:32 +0200

    feat: The module2 has done

--- src/views/card-view.js
+++ src/views/card-view.js
@@ -1,9 +1,10 @@
 import View from './view.js';
 import {html} from '../utils.js';
 
+/**
+ * @extends {View<PointViewState>}
+ */
 class CardView extends View {
-
-
   /**
    * @override
    */
@@ -26,8 +27,10 @@ class CardView extends View {
    * @return {SafeHtml}
    */
   createStartDateHtml() {
+    const point = this.state;
+
     return html`
-      <time class="event__date" datetime="2019-03-18">MAR 19</time>
+      <time class="event__date" datetime="${point.startDateTime}">${point.startDate}</time>
 
     `;
   }
@@ -36,9 +39,12 @@ class CardView extends View {
    * @return {SafeHtml}
    */
   createTypeIconHtml() {
+    const point = this.state;
+    const type = point.types.find((it) => it.isSelected);
+
     return html`
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type.value}.png" alt="Event type icon">
       </div>
     `;
   }
@@ -47,8 +53,13 @@ class CardView extends View {
    * @return {SafeHtml}
    */
   createDestinationHtml() {
+    const point = this.state;
+    const type = point.types.find((it) => it.isSelected);
+    const destination = point.destinations.find((it) => it.isSelected);
+
+
     return html`
-      <h3 class="event__title">Taxi Amsterdam</h3>
+      <h3 class="event__title">${type.value} ${destination.name}</h3>
     `;
   }
 
@@ -83,14 +94,24 @@ class CardView extends View {
    * @return {SafeHtml}
    */
   createOfferListHtml() {
+    const point = this.state;
+    const offers = point.offers.filter((it) => it.isSelected);
+
+    if (!offers.length) {
+      return '';
+    }
+
     return html`
         <h4 class="visually-hidden">Offers:</h4>
         <ul class="event__selected-offers">
-          <li class="event__offer">
-            <span class="event__offer-title">Order Uber</span>
-            +€&nbsp;
-            <span class="event__offer-price">20</span>
-          </li>
+          ${offers.map((it) => html`
+             <li class="event__offer">
+              <span class="event__offer-title">${it.title}</span>
+               +€&nbsp;
+              <span class="event__offer-price">${it.price}</span>
+             </li>
+          `)}
+
         </ul>
     `;
   }
@@ -99,8 +120,9 @@ class CardView extends View {
    * @return {SafeHtml}
    */
   createFaforiteButtonHtml() {
+    const point = this.state;
     return html`
-        <button class="event__favorite-btn event__favorite-btn--active" type="button">
+    <button class="event__favorite-btn ${point.isFavorite ? 'event__favorite-btn--active' : ''}" type="button">
           <span class="visually-hidden">Add to favorite</span>
           <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
             <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"></path>
